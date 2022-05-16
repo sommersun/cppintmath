830c9097e261b145be948aba6e7e15b2e6e95c25
sommersun <50041042+sommersun@users.noreply.github.com>
2022-05-16 15:31:25 +0800

    Update bitAri.cpp

--- bitAri.cpp
+++ bitAri.cpp
@@ -6,3 +6,12 @@ int UnsignedBitCount(cpp_int number){
   }
   return len;
 }
+
+cpp_int UnsignedBitCountCpp(cpp_int number){
+  cpp_int len = 0;
+  while (number != 0) {
+      number = number>>1;
+      len++;
+  }
+  return len;
+}
